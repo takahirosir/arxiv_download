f58810bccb4f2b1d5c64bdc90f130fd49a331b2a
takahirosir <98999068+takahirosir@users.noreply.github.com>
2023-09-15 14:53:06 +0000

    update and download

--- arxiv_download.py
+++ arxiv_download.py
@@ -1,11 +1,16 @@
 import arxiv
+import os
 
 search = arxiv.Search(
   query = "LLM AND medical",
-  max_results = 5,
+  max_results = 150,
   sort_by = arxiv.SortCriterion.SubmittedDate,
   sort_order = arxiv.SortOrder.Descending
 )
 
+newpath = r'./download' 
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
 for result in search.results():
-    result.download_pdf()
+    result.download_pdf(dirpath="./download")
